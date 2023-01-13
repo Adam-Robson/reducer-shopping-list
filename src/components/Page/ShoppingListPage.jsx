7d3324fea3fa1eb6cb1ff14c4793d17b11db44e8
Adam Robson <adamray312@gmail.com>
2023-01-13 11:00:55 -0800

    build list context provider framework

--- src/components/Page/ShoppingListPage.jsx
+++
@@ -1,5 +0,0 @@
-export default function ShoppingListPage() {
-  return <section>
-    <h1>My Shopping List</h1>
-  </section>;
-}
---
+++ src/context/ListContext.jsx
@@ -0,0 +1,8 @@
+import { createContext } from 'react';
+
+export const Context = createContext();
+
+export const ContextProvider = ({ children }) => {
+  return <Context.Provider>{ children }</Context.Provider>;
+};
+
