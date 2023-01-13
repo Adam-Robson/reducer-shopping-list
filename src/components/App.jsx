a83ff0d1e9695eea0627df31a4cf5b1791eaa635
Adam Robson <adamray312@gmail.com>
2023-01-13 10:47:40 -0800

    everything is wired up from db, branch initialized

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,14 @@ import { useEffect } from 'react';
 import Layout from './Page/Layout.jsx';
 import './Design.css';
 import ShoppingListPage from './Page/ShoppingListPage.jsx';
-import { getShoppingListItems } from '../services/shopping-list-items.js';
+import { getItems } from '../services/fetch-utils.js';
 
 export default function App() {
   // This is just to prove we actually can get some items from Supabase without
   // having to wire everything up. This useEffect and everything in it should be
   // removed once that functionality is proven.
   useEffect(() => {
-    getShoppingListItems().then((items) => {
+    getItems().then((items) => {
       console.info('items', items);
     });
   }, []);
---
+++ src/services/fetch-utils.js
@@ -0,0 +1,54 @@
+import { client, checkError } from './client.js';
+
+export async function getItems() {
+  const res = await client
+    .from('reducer')
+    .select(`
+      *
+`)
+    .order('created_at', { ascending: false })
+  ;
+  return checkError(res);
+}
+
+export async function getItem(ItemId) {
+  const res = await client
+    .from('reducer')
+    .select(`
+      *
+`)
+    .match({ id: ItemId })
+    .single()
+  ;
+  return checkError(res);
+}
+
+export async function createItem(item) {
+  const res = await client
+    .from('reducer')
+    .insert({
+      ...item,
+    })
+  ;
+  return checkError(res);
+}
+
+export async function updateItem(itemId, item) {
+  const res = await client
+    .from('reducer')
+    .update({
+      ...item,
+    })
+    .eq('id', itemId)
+  ;
+  return checkError(res);
+}
+
+export async function deleteItem(itemId) {
+  const res = await client
+    .from('reducer')
+    .delete()
+    .eq('id', itemId)
+  ;
+  return checkError(res);
+}
